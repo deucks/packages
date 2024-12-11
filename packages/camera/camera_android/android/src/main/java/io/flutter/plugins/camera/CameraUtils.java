25fcb88fae732ab44adb9c14655ece42e4e470b7
Raajit Sharma <cosmickinect1@gmail.com>
2024-12-11 18:25:22 +1100

    fix import

--- packages/camera/camera_android/android/src/main/java/io/flutter/plugins/camera/CameraUtils.java
+++ packages/camera/camera_android/android/src/main/java/io/flutter/plugins/camera/CameraUtils.java
@@ -17,6 +17,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import android.util.Log;
+import java.util.Arrays;
 
 /** Provides various utilities for camera. */
 public final class CameraUtils {
